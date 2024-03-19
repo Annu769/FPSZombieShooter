89ba97f5bf4e4d818966d43a6e6199af80bbd7eb
Annu769 <109024549+Annu769@users.noreply.github.com>
2024-03-19 12:39:10 +0530

    Add Zombie Mvc
    Add Zombie MVC it help to zombie Move follow Player and more Ai things

--- Assets/Myproject/Scripts/Zombie/ZombieService.cs
+++ Assets/Myproject/Scripts/Zombie/ZombieService.cs
@@ -2,20 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FPSZombie.SingleTon;
+using FPSZombie.ScriptableObjects;
 namespace FPSZombie.Zombie
 {
+    public enum ZombieType
+    {
+        police, monster, lady
+    };
     public class ZombieService :GenericMonoSingleTon<ZombieService>
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] private EnemyScriptableObjectList zombieList;
+        [SerializeField] private Canvas enemyCanvas;
+        private ZombieController zombieController;
+        
 
+        private void Start()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                CreateZombie();
+            }
+            
         }
 
-        // Update is called once per frame
-        void Update()
+        public ZombieController CreateZombie()
         {
-
+            ZombieType zombieType = GetRandomZombie();
+            EnemyScriptableObject zombieData = zombieList.enemy[(int)zombieType];
+            zombieController = new ZombieController(zombieData, enemyCanvas, zombieType);
+                return zombieController;
+        }
+        public ZombieType GetRandomZombie()
+        {
+            return (ZombieType)Random.Range(0, zombieList.enemy.Length);
         }
     }
 }

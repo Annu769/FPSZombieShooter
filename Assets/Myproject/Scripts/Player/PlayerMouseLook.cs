a9ea443881aa6f0a8f22cd2a744c20d364c0f209
Annu769 <109024549+Annu769@users.noreply.github.com>
2024-03-19 11:14:40 +0530

    Add Mvc Implementation for Player
    Add Mvc Implementation for Player it help to player move forward backward and horizomtaly jump as well

---
+++ Assets/Myproject/Scripts/Player/PlayerMouseLook.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseLook : MonoBehaviour
+{
+    [SerializeField] private float sensitivity = 200f;
+    [SerializeField] private Transform player;
+    private float mouseX;
+    private float mouseY;
+    private float rotation;
+    private float minAngle = -90f;
+    private float maxAngle = 90f;
+   
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        rotation -= mouseY;
+        rotation = Mathf.Clamp(rotation, minAngle, maxAngle);
+        transform.localRotation = Quaternion.Euler(rotation, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
+    }
+
+}
